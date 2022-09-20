363b8882c1b05992b0032736a14708b3f27b0595
konbu33 <konbu33@gmail.com>
2022-09-20 11:19:29 +0900

    test: add firebase-functions-test configure

---
+++ functions/src/__tests__/setup.ts
@@ -0,0 +1,27 @@
+import * as functionsTest from 'firebase-functions-test';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+let projectId: string = "";
+let setUpJson: string = "";
+
+try {
+  setUpJson = process.env.SETUP_JSON as string;
+  projectId = process.env.PROJECT_ID as string;
+  
+} catch (e) {
+  console.log(" setup error : ", String(e));
+  
+}
+
+// テスト対象の関関とFirebaseのProjectとを紐付けるために、testConfigを定義する。
+// 利利するFirebaseのサービスによって、引数は変わる。
+export const testConfig = functionsTest(
+  {
+    projectId: projectId,
+  },
+  setUpJson,
+);
+
+
