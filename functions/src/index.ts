3e469643e94912656ca085836fe129d0681236a8
konbu33 <konbu33@gmail.com>
2022-09-20 11:33:21 +0900

    feat: add createRtcIdTokenOnRequest class

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import { helloWorld } from "./hello";
 import { helloWorldSecureOnRequest } from "./hello";
 import { helloWorldSecureOnCall } from "./hello";
+import { createRtcIdTokenOnRequest }  from "./createRtcIdTokenOnRequest";
 
 export {
     helloWorld,
     helloWorldSecureOnRequest, 
     helloWorldSecureOnCall, 
+    createRtcIdTokenOnRequest,
 };
