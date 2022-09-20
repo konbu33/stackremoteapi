c17464906e1eafa9dfa4e92deddba8577de2b7b1
konbu33 <konbu33@gmail.com>
2022-09-20 11:35:25 +0900

    feat: add createRtcIdToken class

---
+++ functions/src/createRtcIdToken.ts
@@ -0,0 +1,140 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); 
+
+import * as functions from "firebase-functions";
+
+import { verifyFirebaseAuthIdTokenOnCall } from './lib/verifyFirebaseAuthIdTokenOnCall';
+import { ValidateRequestData } from './lib/validateRequestData';
+import { ResponseData, ResponseDataState } from './lib/responseData';
+import { createToken } from './lib/createToken';
+import { RequestData } from './lib/requestData';
+import { Result } from './lib/result';
+
+
+export const createRtcIdToken = functions.https.onCall(async (data, context) => {
+  functions.logger.info("start : createRtcIdToken -------------------------------------------------- ", { structuredData: true });
+
+  // --------------------------------------------------
+  // 
+  // Requestデータのオブジェクト生成
+  // 
+  // --------------------------------------------------
+  const requestData: RequestData = data;
+
+  let code: number;
+
+  // --------------------------------------------------
+  //
+  // Firebase Authentication で認証状況確認
+  //
+  // --------------------------------------------------
+  
+  const verifyFirebaseAuthIdTokenResult: Result<string> = await verifyFirebaseAuthIdTokenOnCall(context);
+  const verifyFirebaseAuthUid: string = JSON.parse(verifyFirebaseAuthIdTokenResult.data).uid;
+
+  code = verifyFirebaseAuthIdTokenResult.code;
+  if (code === 500) {
+    console.log("verifyFirebaseAuthIdTokenResult : ", verifyFirebaseAuthIdTokenResult);
+    return verifyFirebaseAuthIdTokenResult;
+  }
+  
+  functions.logger.info("end : verifyFirebaseAuthIdToken", { structuredData: true });
+
+  // --------------------------------------------------
+  //
+  // Requestのデータのバリデーション
+  //
+  // --------------------------------------------------
+
+  // channelName 検証
+  const validateResultChannelNameResult: Result<string> = ValidateRequestData.validateChannelName(requestData);
+  const channelName = validateResultChannelNameResult.data;
+  
+  code = validateResultChannelNameResult.code;
+  if (code === 500) {
+    console.log("validateResultChannelNameResult : ", validateResultChannelNameResult);
+    return validateResultChannelNameResult;
+  }
+
+  functions.logger.info("end : validateChannelName", { structuredData: true });
+
+  // uid 検証
+  const validateResultUidResult: Result<number> = ValidateRequestData.validateUid(requestData);
+  const uid = validateResultUidResult.data;
+
+  code = validateResultUidResult.code;
+  if (code === 500) {
+    console.log("validateResultUidResult : ", validateResultUidResult);
+    return validateResultUidResult;
+  }
+
+  functions.logger.info("end : validateUid", { structuredData: true });
+
+  // role 検証
+  const validateResultRoleResult: Result<number> = ValidateRequestData.validateRole(requestData);
+  const role = validateResultRoleResult.data;
+
+  functions.logger.info("end : validateRole", { structuredData: true });
+
+  // privilegeExpireTime 検証
+  const validateResultPrivilegeExpireTimeResult: Result<number> = ValidateRequestData.validatePrivilegeExpireTime(requestData);
+  const privilegeExpireTime = validateResultPrivilegeExpireTimeResult.data;
+
+  functions.logger.info("end : validatePrivilegeExpireTime", { structuredData: true });
+  // --------------------------------------------------
+  // 
+  // Responseデータのオブジェクト生成
+  // 
+  // --------------------------------------------------
+ 
+  // requestのbodyに含まれている想定のjsonを基に、RtcIdTokenを生成する情情をまとめたデータインスタンス生成
+  const responseDataObject: ResponseData = {
+    channelName: channelName,
+    uid: uid,
+    account: requestData.account,
+    rtcIdTokenType: requestData.rtcIdTokenType,
+    role: role,
+    privilegeExpireTime: privilegeExpireTime,
+    firebaseAuthIdToken: String(context.auth?.token),
+    verifyFirebaseAuthUid: verifyFirebaseAuthUid,
+    rtcIdToken: "undefined",
+  };
+
+  const responseDataState: ResponseDataState = new ResponseDataState(responseDataObject);
+
+  // Token生成し、ResponseDataへ追加
+  const rtcIdTokenResult: Result<string> = createToken(responseDataObject);
+  const rtcIdToken: string = rtcIdTokenResult.data;
+  responseDataState.setRtcIdToken(rtcIdToken);
+
+  code = rtcIdTokenResult.code;
+  if (code === 500) {
+    console.log("rtcIdTokenResult : ", rtcIdTokenResult);
+    return rtcIdTokenResult;
+  }
+
+  functions.logger.info("end : responseData and createToken", { structuredData: true });
+
+  // --------------------------------------------------
+  // 
+  // Responseデータ応答
+  // 
+  // --------------------------------------------------
+  const responseData: ResponseData = responseDataState.state;
+  const responseDataResult: Result<string> = {
+    code: 200,
+    message: "SUCCESS : create rtc id token",
+    data: JSON.stringify(responseData),
+  }
+
+  functions.logger.info("end : responseData ", { structuredData: true });
+  functions.logger.info("end : createRtcIdToken -------------------------------------------------- ", { structuredData: true });
+
+  console.log("responseData : ", responseData);
+  return responseDataResult;
+});
+
+
+
+
+
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,12 @@ import { helloWorld } from "./hello";
 import { helloWorldSecureOnRequest } from "./hello";
 import { helloWorldSecureOnCall } from "./hello";
 import { createRtcIdTokenOnRequest }  from "./createRtcIdTokenOnRequest";
+import { createRtcIdToken } from "./createRtcIdToken";
 
 export {
     helloWorld,
     helloWorldSecureOnRequest, 
     helloWorldSecureOnCall, 
     createRtcIdTokenOnRequest,
+    createRtcIdToken,
 };
